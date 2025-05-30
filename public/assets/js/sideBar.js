5b582778381f945d050cec2bcf391807b77ec6a4
AdaoTchiyombe20 <adaotchiyombe@gmail.com>
2025-05-31 00:21:35 +0200

    Finalizando a home e a pagina de agendamentos

---
+++ public/assets/js/sideBar.js
@@ -0,0 +1,23 @@
+const paths =  [
+    {id:1, name:"home.html" },
+    {id:2, name:"agendamentos.html" },
+    {id:3, name:"medicos.html" },
+    {id:4, name:"historico.html" },
+    {id:5, name:"perfil.html" },
+    {id:6, name:"configuracoes.html" }
+];
+
+const liElements = [
+    document.querySelector(".path.l1"),
+    document.querySelector(".path.l2"),
+    document.querySelector(".path.l3"),
+    document.querySelector(".path.l4"),
+    document.querySelector(".path.l5"),
+    document.querySelector(".path.l6")
+]
+
+ paths.map((path, idx)=> {
+    if( window.location.pathname.includes(path.name)) {
+        liElements[idx].classList.add("selected");
+    }
+})
